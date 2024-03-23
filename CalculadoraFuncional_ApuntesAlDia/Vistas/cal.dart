59c9555b03581744f299850f15043cb380c06730
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2024-03-23 17:48:22 -0600

    Actualizacion con calculadora funcional 23-03-2024

---
+++ CalculadoraFuncional_ApuntesAlDia/Vistas/cal.dart
@@ -0,0 +1,418 @@
+import "package:flutter/material.dart";
+
+class Calculadora extends StatefulWidget {
+  const Calculadora({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Calculadora> createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+
+  String _textPantalla = "0"; // solo se utiliza para mostrar en pantalla
+  String _operador = "";
+  double _trab = 0; // memoria de trabajo (se muestra el tecleo)
+  double _memTotal = 0; // almacena la operacion total (solo muestra el resultado final cuando se ponga igual)
+  bool _hayPunto = false;
+
+  void _borrarTodo() { // Pone en cero la pantalla de la calculadora
+    _hayPunto = false;
+    _memTotal = 0;
+    setState(() {
+      _textPantalla = "0";
+      _operador = "";
+    });
+  }
+
+  void _borrarUno() { // borrar un digito
+    if(_textPantalla != "0" && _operador != "=") { // cuando hay algo que borrar
+      setState(() {
+        if(_textPantalla.endsWith(".") && _textPantalla.length>1) { // cuando se borra un punto flotante
+          _hayPunto = false;
+          _textPantalla = _textPantalla.substring(0,_textPantalla.length -1);
+        } else if (_textPantalla.length == 1) { // cuando solo hay un digito
+          _textPantalla = "0";
+        } else { // cuando hay mas de un digito
+          _textPantalla = _textPantalla.substring(0,_textPantalla.length -1);
+        }
+      });
+    } else if(_textPantalla == "0") { // cuando es cero
+      // no hace nada
+    }
+  }
+
+  void _presionarNumero(int n) {  // Mostrar numero en la pantalla de la calculadora
+    String nStr = n.toString();
+    setState(() {
+      if(_textPantalla == "0"){ // si es el primer numero
+        _textPantalla = nStr;
+      } else {
+        _textPantalla = _textPantalla + nStr;
+      }
+    });
+  }
+
+  void _presionarPunto() { // Agregar decimal
+    if(_hayPunto == false) {
+      _hayPunto = true;
+      setState(() {
+        _textPantalla = _textPantalla + ".";
+      });
+    } else {
+      // no agrega nada si ya existe punto
+    }
+
+
+    //String strTrab = _trab.toString();
+    //strTrab = strTrab.substring(0,strTrab.length);
+    //strTrab = strTrab + ".";
+    //print(strTrab);
+
+
+    //String ejem = "hola.";
+    //print(ejem.endsWith("."));
+    //double ejem1 = 2.0;
+    //double res = ejem1*1.1;
+    //print("numero $ejem1");
+    //if(ejem.endsWith("a")){}
+
+  }
+
+  void _resultado() {
+    //_trab = double.parse(_textPantalla);
+    if(_operador == "*") { // si es multiplicacion
+      _trab = double.parse(_textPantalla);
+      _memTotal = _memTotal * _trab;
+      //print("Memoria multi $_memTotal");
+    } else if(_operador == "+") {
+      _trab = double.parse(_textPantalla);
+      _memTotal = _memTotal + _trab;
+    } else if(_operador == "-") {
+      _trab = double.parse(_textPantalla);
+      _memTotal = _memTotal - _trab;
+    } else if(_operador == "/") {
+      _trab = double.parse(_textPantalla);
+      _memTotal = _memTotal / _trab;
+    } else if(_operador == "=") { // si no hay operacion (se repite el igual)
+      // no hace nada
+      //print("memoria igual $_memTotal");
+    }
+    setState(() {
+      _operador = "=";
+      _textPantalla = _memTotal.toString();
+    });
+    //print("memorial res $_memTotal");
+    //_memTotal = 0;
+  }
+
+  void _division() {
+    _trab = double.parse(_textPantalla);
+    if(_memTotal == 0) {
+      _memTotal = _trab;
+    } else if (_memTotal!=0 && _operador=="=" ){
+      // no hace nada, espera la la operacion siguiente
+    } else {
+      _memTotal = _memTotal / _trab;
+    }
+    setState(() {
+      _operador = "/";
+      _textPantalla = " ";
+      //_textPantalla = "0";
+    });
+  }
+
+  void _multiplicar(){
+    _trab = double.parse(_textPantalla);
+    if(_memTotal == 0) {
+      _memTotal = _trab;
+    } else if (_memTotal!=0 && _operador=="=" ){
+      // no hace nada, espera la la operacion siguiente
+    } else {
+      _memTotal = _memTotal * _trab;
+    }
+    setState(() {
+      _operador = "*";
+      _textPantalla = " ";
+      //_textPantalla = "0";
+    });
+  }
+
+  void _sumar() {
+    _trab = double.parse(_textPantalla);
+
+    if(_operador == "-") {  // Si se suma un negativo a un positivo
+      _trab = -_trab;
+    }
+
+    if(_memTotal == 0) {
+      _memTotal = _trab;
+    } else if (_memTotal!=0 && _operador=="=" ) {
+      // no hace nada, espera la la operacion siguiente
+    } else {
+      _memTotal = _memTotal + _trab;
+    }
+    setState(() {
+      _operador = "+";
+      _textPantalla = " ";
+      //_textPantalla = "0";
+    });
+  }
+
+  void _restar() {
+    _trab = double.parse(_textPantalla);
+    if(_memTotal == 0) {
+      if(_operador == "-") {  // cuando se resten numero negativos
+        _memTotal = _memTotal - _trab;
+      } else {
+        _memTotal = _trab;
+      }
+    } else if (_memTotal!=0 && _operador=="=" ){
+      // no hace nada, espera la la operacion siguiente
+    } else {
+      _memTotal = _memTotal - _trab;
+    }
+    setState(() {
+      _operador = "-";
+      _textPantalla = " ";
+      //_textPantalla = "0";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Container(  // Caja donde se muestra el resultado (pantalla de la calculadora)
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(
+                  width: 2,
+                  color: Colors.black,
+                ),
+              ),
+              height: 60,
+              width:  224,
+              child: Text(
+                _operador +" "+ _textPantalla, //"$_trab",
+                style: TextStyle(fontSize: 40),
+                textAlign: TextAlign.right,
+              ),
+            ),
+
+            Container(  // Contiene los botones de la calculadora (Teclado)
+              height: 280,
+              width:  224,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        onPressed:(){
+                          _multiplicar();
+                        },
+                        child: const Text(
+                          "*",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _division();
+                        },
+                        child: const Text(
+                          "/",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+
+                      FloatingActionButton(
+                        onPressed:(){
+                          _borrarUno();
+                        },
+                        child: const Text(
+                          "C",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _borrarTodo();
+                        },
+                        child: const Text(
+                          "CE",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(7);
+                        },
+                        child: const Text(
+                          "7",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(8);
+                        },
+                        child: const Text(
+                          "8",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(9);
+                        },
+                        child: const Text(
+                          "9",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        backgroundColor: Colors.green,
+                        onPressed:(){
+                          _sumar();
+                        },
+                        child: const Text(
+                          "+",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(4);
+                        },
+                        child: const Text(
+                          "4",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(5);
+                        },
+                        child: const Text(
+                          "5",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(6);
+                        },
+                        child: const Text(
+                          "6",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        backgroundColor: Colors.red,
+                        onPressed:(){
+                          _restar();
+                        },
+                        child: const Text(
+                          "-",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(1);
+                        },
+                        child: const Text(
+                          "1",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(2);
+                        },
+                        child: const Text(
+                          "2",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(3);
+                        },
+                        child: const Text(
+                          "3",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarPunto();
+                        },
+                        child: const Text(
+                          ".",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _presionarNumero(0);
+                        },
+                        child: const Text(
+                          "0",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:(){
+                          _resultado();
+                        },
+                        child: const Text(
+                          "=",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ CalculadoraFuncional_ApuntesAlDia/Vistas/calend.dart
@@ -0,0 +1,172 @@
+import "package:clase2857/Vistas/agregarEvento.dart";
+import "package:flutter/material.dart";
+import "package:syncfusion_flutter_calendar/calendar.dart";
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Calendario extends StatefulWidget {
+  const Calendario({super.key, required this.title});
+
+  final String title;  // la variable tipo Final es un tipo que no se va a cambiar
+
+  @override
+  State<Calendario> createState() => _CalendarioState(); // guion bajo ayuda a encontrar mas facilmente los estados. Los hace privados
+}
+
+class _CalendarioState extends State<Calendario> {
+  final db = FirebaseFirestore.instance;
+  late List<Meeting> meetings = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+    _cargarDatos(); // para cargar los datos de la bd
+  }
+
+  Future<void>  _cargarDatos() async{
+    List<Meeting> m = [];
+    await db.collection("calendario").get().then(
+        (documentos){
+          for(var doc in documentos.docs){
+            final datos = doc.data();
+            final nombre = datos["Nombre"];
+            final inicio = (datos["Inicio"] as Timestamp).toDate();
+            final fin = (datos["Fin"] as Timestamp).toDate();
+            final color = Color(datos["Color"]);
+            m.add(
+              Meeting(nombre, inicio, fin, color, false)
+            );
+          }
+        }
+    );
+    setState(() {
+      meetings = m;
+    });
+  }
+
+  /*  Ya no se usa porque se genero este proceso manualmente
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));  // aqui se define el evento del calendario, como la conferencia
+    meetings.add(
+      Meeting('Conference', startTime, endTime, const Color(0xFF0F8644), false),); // cada uno de estos es un evento en el calendario
+    return meetings;
+  }
+  */
+
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold y floattingActionButton se quedan fijos (como superpuestos) sin importar a donde movamos la pantalla. (ImgPW1)
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SfCalendar(
+          dataSource: MeetingDataSource(meetings),
+          view: CalendarView.month,
+          firstDayOfWeek: 1,
+          monthViewSettings: MonthViewSettings(showAgenda: true),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(  // boton para agregar un evento
+          onPressed: (){
+            showDialog(
+              context: context,
+              builder: (BuildContext context){
+                return const AgregarEvento(title: "Nuevo evento");
+              }
+            );
+          },
+          child: const Icon(Icons.add),
+      ),
+
+
+
+
+
+
+
+      /*
+      floatingActionButton:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.end,  // Alineacion del texto
+        children: <Widget>[  // Lista de widgets
+
+        ],
+      ),
+      */
+
+
+    );
+  }
+
+
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
+  }
+}
+
+class Meeting {
+  /// Creates a meeting class with required details.
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  /// Event name which is equivalent to subject property of [Appointment].
+  String eventName;
+
+  /// From which is equivalent to start time property of [Appointment].
+  DateTime from;
+
+  /// To which is equivalent to end time property of [Appointment].
+  DateTime to;
+
+  /// Background which is equivalent to color property of [Appointment].
+  Color background;
+
+  /// IsAllDay which is equivalent to isAllDay property of [Appointment].
+  bool isAllDay;
+}
